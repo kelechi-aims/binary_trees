327ec8c670563d31374ae0258fddb7d2b8e518fb
Tolulope Abiola <anotibills@gmail.com>
2023-08-30 10:46:18 +0100

    Taks 10 resolved

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - mfunction that measures the depth of a node in a binary tree.
+ * binary_tree_depth - function that measures depth of a node in a binary tree
  * @tree: a pointer to the root
  * Return: if NULL, return 0.
  */
