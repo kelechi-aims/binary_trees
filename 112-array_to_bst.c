32d551ece6ec135be78966adeb725158893dc805
kelechi-aims <kelechi@aims.edu.gh>
2023-08-30 14:21:16 +0100

    task-26

---
+++ 112-array_to_bst.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - Builds a Binary Search Tree from an array.
+ * @array: A pointer to the first element of the array to be converted.
+ * @size: The number of elements in the array. *
+ * Return: A pointer to the root node of the created BST, or NULL on failure.
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *root = NULL;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		if (bst_insert(&root, array[i]) == NULL)
+		{
+			/* Handle insertion failure */
+			/* You might want to free previously allocated memory here */
+			return (NULL);
+		}
+	}
+	return (root);
+}
