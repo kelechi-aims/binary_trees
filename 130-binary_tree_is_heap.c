a7b148257e219c0af3b70f7b3ad10d11c3c9309d
Tolulope Abiola <anotibills@gmail.com>
2023-08-31 20:18:59 +0100

    heap again

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -2,30 +2,9 @@
 #include "102-binary_tree_is_complete.c"
 
 /**
- * binary_tree_is_max_heap - func that checks if a binary tree is a max heap
+ * binary_tree_is_heap - function that checks if a binary tree is a max heap
  * @tree: pointer to the root
- * @max_value: maximum value that the tree
  * Return: 1 if max heap, 0 otherwise
- **/
-int binary_tree_is_max_heap(const binary_tree_t *tree, int max_value)
-{
-	if (!tree)
-		return 1;
-
-	if (tree->n >= max_value)
-		return 0;
-
-	if (!binary_tree_is_max_heap(tree->left, tree->n) ||
-	    !binary_tree_is_max_heap(tree->right, tree->n))
-		return 0;
-
-	return 1;
-}
-
-/**
- * binary_tree_is_heap - func checks if a binary tree is a heap
- * @tree: pointer to the root
- * Return: 1 if a heap, 0 otherwise
  **/
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
@@ -35,6 +14,19 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 	if (!binary_tree_is_complete(tree))
 		return 0;
 
-	return binary_tree_is_max_heap(tree, INT_MAX);
+	if (tree->left)
+	{
+		if (tree->left->n > tree->n)
+			return 0;
+		if (!binary_tree_is_heap(tree->left))
+			return 0;
+	}
+	if (tree->right)
+	{
+		if (tree->right->n > tree->n)
+			return 0;
+		if (!binary_tree_is_heap(tree->right))
+			return 0;
+	}
+	return 1;
 }
-
